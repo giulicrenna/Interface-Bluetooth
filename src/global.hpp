15cd8b847ff6ee321008393002fca095db3b91a8
giuliano-crenna <59456310+giulicrenna@users.noreply.github.com>
2022-10-20 23:29:44 -0300

    Bluethooth BLE and pin setup
    - Bluethooth BLE.
    - data sending over UART.
    - pin setup.
    - RGB led function

--- src/global.hpp
+++ src/global.hpp
@@ -6,9 +6,13 @@
 #define TXD_485 28
 #define MAX485_RE_DE 33
 #define RX_BUFF_SIZE 30
+#define PIN_RED    20
+#define PIN_GREEN  21
+#define PIN_BLUE   22
 
 const char *deviceID = std::to_string(ESP.getEfuseMac()).c_str();
 
+int pin = 741963;
 uint64_t currentTimeBluethoothMessage = 0;
 uint64_t bluethoothMessageTime = 1000;
 
