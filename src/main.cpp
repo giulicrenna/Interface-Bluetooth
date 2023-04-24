39a18504adf413a6ea16242d4cc67ce1b3954e62
giuliano-crenna <59456310+giulicrenna@users.noreply.github.com>
2023-04-24 10:18:07 -0300

    Update
    1. Lexator header.
    2. New states and error messages.
    3. New default rx_timeout.

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 #include "BLE_configs.hpp"
 #include "traditionalBlue.hpp"
 #include "autoBaudrate.hpp"
+#include "lexator.hpp"
 // #include "rgbLeds.hpp"
 
 int previousMillis = 0;
@@ -114,7 +115,7 @@ void task2(void *parameters)
 
         case BLUE_ASK_KEY:
         {
-            askKey();
+            askKey(); // TO MANAGE_DEVICE_CONFIGS
             break;
         }
 
@@ -371,7 +372,9 @@ void initUART()
     Serial.end();
     Serial.begin(UARTparam.baud, UARTparam.parity, UARTparam.rxd, UARTparam.txd, UARTparam.inverted, UARTparam.timeout);
     if (couldDetectUartConfig)
+    {
         Serial.onReceiveError(onRxInterrups);
+    };
     Serial.setRxBufferSize(INCOME_BUFFER * 2);
     Serial.setTimeout(UARTparam.timeout);
 }
@@ -466,7 +469,7 @@ void readFromUART()
 
     if (millis() - currentCheckAscii > 10000)
     {
-        if (detectNonAscii(msg_) && couldDetectUartConfig)
+        if (couldDetectUartConfig && detectNonAscii(msg_))
         {
             currentState = CHANGE_UART_CONFIG;
             return;
@@ -509,20 +512,21 @@ void setupPreferences()
 {
     config.begin("config", false);
 
-    INCOME_BUFFER = config.getInt("buffer", 32);
+    INCOME_BUFFER = config.getInt("buffer", 64);
     sendTime = config.getInt("time", 1000);
     sendToDevice = config.getBool("send", true);
     pinc = config.getString("pinc", master);
 
-    UARTparam.baud = config.getInt("baud", 1200);
+    UARTparam.baud = config.getInt("baud", 115200);
     UARTparam.isAuto = config.getBool("isAuto", true);
     UARTparam.isRS232 = config.getBool("isRS232", true);
+    couldDetectUartConfig = config.getBool("couldDetectUartConfig", false);
     couldDetectUartConfig = config.getBool("couldDetectUartConfig", false);
     if (!UARTparam.isAuto)
     {
         UARTparam.parity = config.getInt("parity", SERIAL_8N1);
     }
-    UARTparam.timeout = config.getInt("rx_timeout", 100);
+    UARTparam.timeout = config.getInt("rx_timeout", 2000);
     petitionMode = config.getBool("petitionMode", true);
 }
 
@@ -548,267 +552,6 @@ void sendTest()
             currentState = BLUE_PAIRING;
         }
         currentTimeBluetoothTestMessage = millis();
-    }
-}
-
-void lexator()
-{
-    if (SerialBT.available() > 0)
-    {
-        String command = SerialBT.readString();
-        command.trim();
-        if (command == commandList.cmd_1)
-        {
-            sendToDevice = false;
-            SerialBT.println(debugging.sta_1);
-            // break;
-        }
-        else if (command == commandList.cmd_2)
-        {
-            sendToDevice = true;
-            config.putBool("send", true);
-            SerialBT.println(debugging.sta_2);
-        }
-        else if (command == commandList.cmd_3)
-        {
-            petition = true;
-        }
-        else if (command == commandList.cmd_4)
-        {
-            config.clear();
-        }
-        else if(command == commandList.help){
-            Blue_send("\n----COMMANDS HELP----\n");
-            Blue_send(commandList.cmd_1_exp);
-            Blue_send(commandList.cmd_2_exp);
-            Blue_send(commandList.cmd_3_exp);
-            Blue_send(commandList.cmd_4_exp);
-            Blue_send(commandList.cmd_5_exp);
-            Blue_send(commandList.cmd_6_exp);
-            Blue_send(commandList.cmd_7_exp);
-            Blue_send(commandList.cmd_8_exp);
-            Blue_send(commandList.cmd_9_exp);
-            Blue_send(commandList.cmd_9_exp);
-            Blue_send(commandList.cmd_10_exp);
-            Blue_send(commandList.cmd_11_exp);
-            Blue_send(commandList.cmd_12_exp);
-            Blue_send(commandList.cmd_13_exp);
-            Blue_send(commandList.cmd_14_exp);
-            Blue_send("\n-------------------\n");
-        }
-        else
-        {
-            String cmd[5];
-            std::vector<std::string> cmd_v = mstd::strip(command.c_str(), ':');
-            for (int k = 0; k < cmd_v.size(); k++)
-            {
-                cmd[k] = String(cmd_v.at(k).c_str());
-            }
-            if (cmd[0] == commandList.cmd_5)
-            {
-                try
-                {
-                    int temp_time = std::stoi(cmd[1].c_str());
-                    sendTime = temp_time;
-                    config.putInt("time", sendTime);
-                    SerialBT.println(debugging.sta_3);
-                }
-                catch (const std::exception &e)
-                {
-                    SerialBT.println(debugging.err_4);
-                    // break;
-                }
-            }
-            else if (cmd[0] == commandList.cmd_6)
-            {
-                try
-                {
-                    int temp_buffer = std::stoi(cmd[1].c_str());
-                    INCOME_BUFFER = temp_buffer;
-                    config.putInt("buffer", INCOME_BUFFER);
-                    SerialBT.println(debugging.sta_4);
-                }
-                catch (const std::exception &e)
-                {
-                    SerialBT.println(debugging.err_4);
-                    // break;
-                }
-            }
-            else if (cmd[0] == commandList.cmd_7)
-            {
-                try
-                {
-                    int timeout = std::stoi(cmd[1].c_str());
-                    config.putInt("rx_timeout", timeout);
-                    UARTparam.timeout = timeout;
-                    SerialBT.println(debugging.sta_4);
-                    currentState = INIT_UART;
-                }
-                catch (const std::exception &e)
-                {
-                    SerialBT.println(debugging.err_4);
-                    // break;
-                }
-            }
-            else if (cmd[0] == commandList.cmd_8)
-            {
-                try
-                {
-                    if (cmd[1] == pinc)
-                    {
-                        pinc = cmd[2].c_str();
-                        config.putString("pinc", cmd[2]);
-                        SerialBT.println(debugging.sta_6);
-                        // break;
-                    }
-                    else
-                    {
-                        SerialBT.println(debugging.err_5);
-                        // break;
-                    }
-                }
-                catch (const std::exception &e)
-                {
-                    SerialBT.println(debugging.err_6);
-                }
-            }
-            else if (cmd[0] == commandList.cmd_9)
-            {
-                config.putString("pinc", master);
-                Blue_send(debugging.sta_11);
-            }
-            else if (cmd[0] == commandList.cmd_10)
-            {
-                ESP.restart();
-            }
-            else if (cmd[0] == commandList.cmd_14)
-            {
-                String Status = "\n--------------STATUS---------------\n";
-                Status += "BAUDRATE: " + String(UARTparam.baud) + "\n";
-                Status += "PARIDAD: " + UARTparam.parity_str + "\n";
-                if (UARTparam.isRS232)
-                {
-                    Status += "PROTOCOLO: RS232\n";
-                }
-                else
-                {
-                    Status += "PROTOCOLO: RS485\n";
-                }
-                if (UARTparam.isAuto)
-                {
-                    Status += "DETECCION DE BAUDIOS: ACTIVADA\n";
-                }
-                else
-                {
-                    Status += "DETECCION DE BAUDIOS: DESACTIVADA\n";
-                }
-                if (sendToDevice)
-                {
-                    Status += "ESTADO DE TRANSMISION: ENVIANDO\n";
-                }
-                else
-                {
-                    Status += "ESTADO DE TRANSMISION: PAUSADA\n";
-                }
-                if (petitionMode)
-                {
-                    Status += "MODO DE TRABAJO: POR PETICION\n";
-                }
-                else
-                {
-                    Status += "MODO DE TRABAJO: ENVIO AUTOMATICO\n";
-                }
-
-                Status += "UUID: " + String(std::to_string(ESP.getEfuseMac()).c_str()) + "\n";
-                Status += "TAMAÑO DEL BUFFER: " + String(std::to_string(INCOME_BUFFER).c_str()) + "\n";
-                Status += "TIEMPO DE ENVIO: " + String(std::to_string(sendTime).c_str()) + "\n";
-                Status += "-----------------------------------\n";
-                Blue_send(Status);
-            }
-            else if (cmd[0] == commandList.cmd_11)
-            {
-                if (cmd[1] == commandList.cmd_11_param_1)
-                {
-                    UARTparam.isAuto = true;
-                    UARTparam.parity = SERIAL_8N1;
-                    config.putBool("isAuto", UARTparam.isAuto);
-                    SerialBT.println(debugging.sta_8);
-                    currentState = DETERMINATE_BAUD_232_NI;
-                    return;
-                }
-                else
-                {
-                    UARTparam.baud = std::stoi(cmd[1].c_str());
-                    UARTparam.isAuto = false;
-                    config.putInt("baud", UARTparam.baud);
-                    config.putBool("isAuto", UARTparam.isAuto);
-                    try
-                    {
-                        if (cmd[2] == "RS232")
-                        {
-                            UARTparam.isRS232 = true;
-                        }
-                        else if (cmd[2] == "RS232")
-                        {
-                            UARTparam.isRS232 = false;
-                        }
-                        config.putBool("isRS232", UARTparam.isRS232);
-                    }
-                    catch (const std::exception &e)
-                    {
-                        SerialBT.println(debugging.sta_9);
-                    }
-                    determinateParity(cmd[3]);
-                    config.putInt("parity", UARTparam.parity);
-                    SerialBT.println(debugging.sta_9 + cmd[1]);
-                }
-                currentState = MANAGE_DEVICE_CONFIGS;
-            }
-            else if (cmd[0] == commandList.cmd_12_exp)
-            {
-                if (cmd[1] == commandList.cmd_12_param_1)
-                {
-                    petitionMode = false;
-                    config.putBool("petitionMode", petitionMode);
-                    currentState = SEND_BY_TIME;
-                    SerialBT.println(debugging.sta_12);
-                }
-                else if (cmd[1] == commandList.cmd_12_param_2)
-                {
-                    petitionMode = true;
-                    config.putBool("petitionMode", petitionMode);
-                    currentState = SEND_BY_PETITION;
-                    SerialBT.println(debugging.sta_12);
-                }
-                else
-                {
-                    SerialBT.println(debugging.err_7);
-                }
-            }
-            else if (cmd[0] == commandList.cmd_13)
-            {
-                if (cmd[1] == commandList.cmd_13_param_1)
-                {
-                    couldDetectUartConfig = true;
-                    config.putBool("couldDetectUartConfig", couldDetectUartConfig);
-                }
-                else if (cmd[1] == commandList.cmd_13_param_2)
-                {
-                    couldDetectUartConfig = false;
-                }
-                else
-                {
-                    SerialBT.println(debugging.err_7);
-                    config.putBool("couldDetectUartConfig", couldDetectUartConfig);
-                }
-            }
-            else
-            {
-                SerialBT.println(debugging.err_7);
-            }
-
-            // break;
-        }
     }
 }
 
